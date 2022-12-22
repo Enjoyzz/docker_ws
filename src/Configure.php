f485ba763a37bfa4b20cf0a1252948ec1fb29517
enjoys <deadly.info@gmail.com>
2022-12-22 11:46:46 +0300

    fix path

--- src/Configure.php
+++ src/Configure.php
@@ -178,8 +178,8 @@ final class Configure extends Command
      */
     private function copyFilesInRootDirectory(): void
     {
-        copyDirectoryWithFilesRecursive(__DIR__ . '/../.data', Variables::$rootPath . '/.data');
-        CreateSymlink(Variables::$rootPath . '/bin/docker', __DIR__ . '/../bin/docker');
+        copyDirectoryWithFilesRecursive(Variables::FILES_DIR .'/.data', Variables::$rootPath . '/.data');
+        CreateSymlink(Variables::$rootPath . '/bin/docker', Variables::FILES_DIR .'/bin/docker');
     }
 
     /**

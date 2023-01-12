7cc864ebcb75527980057f83fe8167e342e70847
Enjoys <deadly.info@gmail.com>
2023-01-12 18:28:29 +0300

    fix DEFAULT_GOAL

--- src/docker.mk
+++ src/docker.mk
@@ -22,7 +22,7 @@ ifeq ("$(wildcard $(DOCKER_COMPOSE_YAML))","")
 endif
 
 # @see https://www.thapaliya.com/en/writings/well-documented-makefiles/
-DEFAULT_GOAL := help
+.DEFAULT_GOAL := help
 help:
 	@awk 'BEGIN {FS = ":.*##"; printf "Usage:\n  make \033[36m<target>\033[0m\n\nTargets:\n"} /^[a-zA-Z0-9_-]+:.*?##/ { printf "  \033[36m%-40s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
 

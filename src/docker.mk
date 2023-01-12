59dbf7c3f736b9a5f2091e5362cd883528d5a6d8
Enjoys <deadly.info@gmail.com>
2023-01-12 13:54:06 +0300

    improved error message

--- src/docker.mk
+++ src/docker.mk
@@ -1,4 +1,11 @@
 ##@ Docker compose commands
+
+# bash colors
+RED:=\033[0;31m
+GREEN:=\033[0;32m
+YELLOW:=\033[0;33m
+NO_COLOR:=\033[0m
+
 export __UNAME = $(shell id -un)
 export __UID = $(shell id -u)
 export __GID = $(shell id -g)
@@ -19,7 +26,7 @@ DOCKER_COMPOSE = docker-compose \
 	--env-file $(DOCKER_PATH)/.env
 
 ifeq ("$(wildcard $(DOCKER_COMPOSE_YAML))","")
-	ERROR_DOCKER_COMPOSE_YAML = The file $(DOCKER_COMPOSE_YAML) not exists - Try you run `cd .. && make $(MAKECMDGOALS)`
+	ERROR_DOCKER_COMPOSE_YAML = "Error! The file $(DOCKER_COMPOSE_YAML) not exists\nTry you run: $(GREEN)cd .. && make $(MAKECMDGOALS)$(NO_COLOR)"
 endif
 
 # @see https://www.thapaliya.com/en/writings/well-documented-makefiles/
@@ -29,7 +36,7 @@ help:
 
 
 check/docker-compose-file:
-	@$(if $(ERROR_DOCKER_COMPOSE_YAML),$(error $(ERROR_DOCKER_COMPOSE_YAML)))
+	@$(if $(ERROR_DOCKER_COMPOSE_YAML), (echo $(ERROR_DOCKER_COMPOSE_YAML); exit 1))
 
 check/all-checks: check/docker-compose-file
 
